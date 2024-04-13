3ddf112d72b22d12d5079bd05c3a07a70667fa12
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-04-13 12:25:08 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,10 +9,7 @@ st.markdown('<style>div.block-container{padding-top:1rem;}</style', unsafe_allow
 
 
 df = pd.read_excel('Alice.xlsx')
-print(df)
-df.columns = ['Date', 'Time', 'Champ', 'Home', 'Away', 'Pays', 'Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']
 
-#st.write(df)
 create_data = {'Pays': 'multiselect'}
 all_widgets = sp.create_widgets(df[['Pays', 'Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']], create_data)
 df = sp.filter_df(df, all_widgets)
