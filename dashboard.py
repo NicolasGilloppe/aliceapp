f425682243496207e4889162eae956954e756435
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-04-13 12:07:44 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,16 @@
+import mysql.connector
 import streamlit as st
 import streamlit_pandas as sp
 import pandas as pd
 import mysql
 from mysql import connector
 from sqlalchemy import create_engine, MetaData
+
+# Config
+st.set_page_config(page_title='Alice Predictions', page_icon=':bar_chart', layout='wide')
+st.title("Today's Matchs")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style', unsafe_allow_html=True)
+
 
 # Create DB Connection
 db_config = {
@@ -12,19 +19,13 @@ db_config = {
     'host': 'localhost',
     'database': 'alice'
 }
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
 
-engine = create_engine('mysql+mysqlconnector://root:ImProTiik28@localhost/alice')
-query = 'Select * From proba_1'
-
-# Sample dataframe
-df = pd.read_sql(query, engine)
-
-# Config
-st.set_page_config(page_title='Alice Predictions', page_icon=':bar_chart', layout='wide')
-st.title("Today's Matchs")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style', unsafe_allow_html=True)
+my_db = mysql.connector.connect(user = 'root', password = 'ImProTiik28', host = 'localhost', database = 'alice')
+mycursor = my_db.cursor()
+mycursor.execute("Select * From proba_1")
+result = mycursor.fetchall()
+df = pd.DataFrame(result)
+df.columns = ['Date', 'Time', 'Champ', 'Home', 'Away', 'Pays', 'Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']
 
 #st.write(df)
 create_data = {'Pays': 'multiselect'}
@@ -34,4 +35,7 @@ df = sp.filter_df(df, all_widgets)
 
 # Plotting
 st.write(df)
+
+
+
 
