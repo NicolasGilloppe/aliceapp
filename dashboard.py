9a9a47a1785f27ac1eb10429ac85bfcd3db83ce8
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-04-17 19:57:13 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -162,21 +162,10 @@ def main():
 
 
 
-"""if __name__ == '__main__':
-    main()"""
-
-import streamlit as st
-from st_paywall import add_auth
+if __name__ == '__main__':
+    main()
 
 
-
-st.set_page_config(layout='wide', page_icon=':bar_chart', page_title='Alice')
-st.title('Home Page')
-
-add_auth(required=True)
-
-
-st.write('Thanks for joining us!!!!')
 
 
 
