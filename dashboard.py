e40469136bc129d7f347eefbceab561079555e08
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-04-18 16:34:00 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,8 @@ import streamlit_pandas as sp
 import pandas as pd
 import plotly.express as px
 from pymongo.mongo_client import MongoClient
+import datetime
+import pytz
 
 
 @st.cache_data(ttl=3600)
@@ -25,44 +27,51 @@ def main():
         st.write('Here You can find data-driven predictions on your daily football games!')   
     if choice == 'Predictions':
         st.subheader('Predictions')
-        round_cols = ['Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']
-        df[round_cols] = df[round_cols].round(2)
-        champ_by_bookie = {'Unibet': ['Germany', 'Germany2', 'England', 'England2', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Scotland', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Portugal2', 'Sweden', 'Switzerland', 'Turkey'],
-                   'Stake': ['England', 'England2', 'England3', 'England4', 'Germany', 'Germany2', 'Spain', 'Spain2', 'Italy', 'Italy2', 'France', 'France2', 'Netherlands', 'Netherlands2', 'Portugal', 'Portugal2', 'Argentina', 'Austria', 'Australia', 'Brazil', 'Brazil2', 'Chile', 'China', 'Denmark', 'Japan', 'Japan2', 'Mexico', 'Norway', 'Saudi Arabia', 'Scotland', 'Sweden', 'Switzerland', 'Turkey', 'Usa', 'Croatia'],
-                   'Betclic': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Turkey', 'Switzerland'],
-                   'Winamax': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Switzerland', 'Turkey']
-                   }
+        tz = pytz.timezone('Europe/Paris')
+        today = datetime.datetime.now(tz).date()
+        today = today.strftime('%d-%m-%y')
 
-        bookmaker_options = ['Stake', 'Unibet', 'Betclic', 'Winamax']
-        selected_bookmakers = st.multiselect("Select Bookmaker", bookmaker_options)
-        champ_list = []
-        for book in selected_bookmakers:
-            champs = champ_by_bookie[book]
-            for _ in champs:
-                if _ not in champ_list:
-                    champ_list.append(_)
-
-        create_data = {'Pays': 'multiselect'}
-        all_widgets = sp.create_widgets(df[['Pays', 'Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']], create_data)
-        df = sp.filter_df(df, all_widgets)
-        df = df[df['Pays'].isin(champ_list)]
-        
-        for col in round_cols:
-            df[col] = df[col].apply(lambda x: f"{x:.2f}")
-
-        st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
-        
-        lexique = pd.DataFrame(
-            {'Alice Naming': ['Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 
-                            'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15'],
-            'Bet Signification': ['Home team win', 'Draw', 'Away team win', 'Home or Draw', 'Draw or Away',
-                            'Over 2.5 goal', 'Under 2.5 goal', 'Both Team To Score', 'One Or Both Team To Not Score',
-                            'Home win & Over 1.5 goal', 'Away win & over 1.5 goal']}
-        )
-
-        check_lex = st.checkbox(label='Show Bets Signification')
-        if check_lex:
-            st.markdown(lexique.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+        if df['Date'][0] != today:
+            st.write('There is no matchs today!')
+        else:
+            round_cols = ['Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']
+            df[round_cols] = df[round_cols].round(2)
+            champ_by_bookie = {'Unibet': ['Germany', 'Germany2', 'England', 'England2', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Scotland', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Portugal2', 'Sweden', 'Switzerland', 'Turkey'],
+                       'Stake': ['England', 'England2', 'England3', 'England4', 'Germany', 'Germany2', 'Spain', 'Spain2', 'Italy', 'Italy2', 'France', 'France2', 'Netherlands', 'Netherlands2', 'Portugal', 'Portugal2', 'Argentina', 'Austria', 'Australia', 'Brazil', 'Brazil2', 'Chile', 'China', 'Denmark', 'Japan', 'Japan2', 'Mexico', 'Norway', 'Saudi Arabia', 'Scotland', 'Sweden', 'Switzerland', 'Turkey', 'Usa', 'Croatia'],
+                       'Betclic': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Turkey', 'Switzerland'],
+                       'Winamax': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Switzerland', 'Turkey']
+                       }
+    
+            bookmaker_options = ['Stake', 'Unibet', 'Betclic', 'Winamax']
+            selected_bookmakers = st.multiselect("Select Bookmaker", bookmaker_options)
+            champ_list = []
+            for book in selected_bookmakers:
+                champs = champ_by_bookie[book]
+                for _ in champs:
+                    if _ not in champ_list:
+                        champ_list.append(_)
+    
+            create_data = {'Pays': 'multiselect'}
+            all_widgets = sp.create_widgets(df[['Pays', 'Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']], create_data)
+            df = sp.filter_df(df, all_widgets)
+            df = df[df['Pays'].isin(champ_list)]
+            
+            for col in round_cols:
+                df[col] = df[col].apply(lambda x: f"{x:.2f}")
+    
+            st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+            
+            lexique = pd.DataFrame(
+                {'Alice Naming': ['Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 
+                                'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15'],
+                'Bet Signification': ['Home team win', 'Draw', 'Away team win', 'Home or Draw', 'Draw or Away',
+                                'Over 2.5 goal', 'Under 2.5 goal', 'Both Team To Score', 'One Or Both Team To Not Score',
+                                'Home win & Over 1.5 goal', 'Away win & over 1.5 goal']}
+            )
+    
+            check_lex = st.checkbox(label='Show Bets Signification')
+            if check_lex:
+                st.markdown(lexique.style.hide(axis="index").to_html(), unsafe_allow_html=True)
 
     elif choice == 'Alice Historical Datas':
         st.subheader('Alice Performance')
