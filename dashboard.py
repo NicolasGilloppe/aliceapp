e04212e59b0bee7f6a3da0740b9b751a1f3af580
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-04-18 12:34:07 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,10 +5,6 @@ from st_mongo_connection import MongoDBConnection
 
 
 connection = st.connection("mongodb", type=MongoDBConnection)
-db = connection["alicedb"]
-collection = db["users"]
-
-
 data = list(collection.find({}))
 df = pd.DataFrame(data)
 st.write(df)
