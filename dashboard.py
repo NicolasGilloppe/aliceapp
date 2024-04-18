09c55a21f62d029016c2fb8d2e1567cab9456df6
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-04-18 12:07:41 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,12 @@
 from pymongo.mongo_client import MongoClient
 import pandas as pd
 import streamlit as st
+from st_mongo_connection import MongoDBConnection
 
 uri = "mongodb+srv://nicolasgilloppe:s0S8eaYt0mIMdYE7@alicedb.eqrplwk.mongodb.net/?retryWrites=true&w=majority&appName=alicedb"
 
-# Create a new client and connect to the server
-cluster = MongoClient(uri, connectTimeoutMS=30000, socketTimeoutMS=30000)
-db= cluster["alicedb"]
-collection = db['Alicetest']
 
-#Get Datas from Mongo to Df
-data = list(collection.find({}))
-
+connection = st.connection("mongodb", type=MongoDBConnection)
+data = list(connection.find({}))
 df = pd.DataFrame(data)
-
 st.write(df)
