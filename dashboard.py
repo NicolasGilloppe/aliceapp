b3276f4eafc27a03837246b61611690d2567cef2
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-04-18 12:16:44 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,18 +3,10 @@ import pandas as pd
 import streamlit as st
 from st_mongo_connection import MongoDBConnection
 
-connection = st.connection(
-    "mongodb",
-    url="mongodb+srv://nicolasgilloppe:s0S8eaYt0mIMdYE7@alicedb.eqrplwk.mongodb.net/",
-    database="alicedb",
-    collection="Alicetest",
-    kwargs={
-        "retryWrites": "true",
-        "w": "majority",
-        "maxIdleTimeMS": 180000,
-        "serverSelectionTimeoutMS": 2000
-    }
-)
+
+connection = st.connection("mongodb", type=MongoDBConnection)
+
+
 data = list(connection.find({}))
 df = pd.DataFrame(data)
 st.write(df)
