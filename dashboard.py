20bfb1dd922a292528432e192cee5fef598ef50c
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-05-09 18:35:12 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -182,7 +182,7 @@ def main():
                                 user_picks.at[index, 'Bookmaker'] = book
 
                     for index, row in user_picks.iterrows():
-                        st.link_button(label= f"{row['Time']}: {row['Home']} vs {row['Away']}, bet {bk*(float(row['Coeff'])/100)}€ on {row['Bets']} @ {row['Odd']}. Bookmaker is {row['Bookmaker']}", url= f"{row[f'{book}_Url']}")
+                        st.link_button(label= f"{row['Time']}: {row['Home']} vs {row['Away']}, bet {round(bk*(float(row['Coeff'])/100), 2)}€ on {row['Bets']} @ {row['Odd']}. Bookmaker is {row['Bookmaker']}", url= f"{row[f'{book}_Url']}")
                 except ValueError:
                     st.warning('Veuillez entrer une Bankroll valide')
                 
