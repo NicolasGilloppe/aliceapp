5df5fcdd9d1d910c5213bb18a6486f0412050dbb
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-05-20 18:55:29 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,7 @@ import re
 import datetime
 import plotly.express as px
 import bcrypt
+import plotly.graph_objects as go
 
 def verify_password(password, hashed_password):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
