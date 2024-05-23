24a9e318bb00e235e50291be141a0995be753f21
NicolasGilloppe <143018427+NicolasGilloppe@users.noreply.github.com>
2024-05-23 17:57:22 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,25 @@ import plotly.express as px
 import bcrypt
 import plotly.graph_objects as go
 
+def get_max(df, bet):
+    columns = [f'Stake_{bet}', f'Unibet_{bet}', f'Betclic_{bet}', f'Winamax_{bet}']
+    values = [float(df[col].values[0]) for col in columns]
+    max_value = max(values)
+    max_url = f"{columns[values.index(max_value)].replace(f'_{bet}', '')}_Url"
+    name = max_url.replace('_Url', '')
+    return max_value, max_url, name
+
+def get_odds(df, bet):
+    columns = [f'Stake_{bet}', f'Unibet_{bet}', f'Betclic_{bet}', f'Winamax_{bet}']
+    values = [float(df[col].values[0]) for col in columns]
+
+    return values
+
+
+def add_space(i):
+    for _ in range(1, i):
+        st.write('')
+
 def verify_password(password, hashed_password):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
 
@@ -73,15 +92,13 @@ def signup():
         elif password and conf_password and password != conf_password:
             st.warning('Please enter the same password')
         elif password and conf_password and email and password == conf_password and name:
-            insert_datas('UsersDb', 'Users', {'_id': email, 'name': name, 'password': hash_password(password), 'Books': books, 'Join': datetime.datetime.today().strftime('%d-%m-%y')})
+            insert_datas('UsersDb', 'Users', {'_id': email, 'name': name, 'password': hash_password(password), 'Books': books})
             st.write('Your Account Has Been Created Succesfully. You Can Now Login')
 
 def main():
     st.write("<h2 style='text-align: center; font-size: 80px;'>Welcome to Alice</h2>", unsafe_allow_html=True)
     st.write("<h2 style='text-align: center; font-size: 15px; color: cyan;'>Your-all-in one personal betting algorithm</h2>", unsafe_allow_html=True)
-    st.write('')
-    st.write('')
-    st.write('')
+    add_space(4)
 
     session_state = get_session_state()
 
@@ -100,9 +117,20 @@ def main():
         user_name = user_data.get('name')
         user_username = user_data.get('_id')
         user_bookies = user_data['Books']
-        user_join = user_data['Join']
         df = get_datas('alicedb', 'Alice_1')
 
+        champ_by_bookie = {'Unibet': ['Germany', 'Germany2', 'England', 'England2', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Scotland', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Portugal2', 'Sweden', 'Switzerland', 'Turkey', 'Saudi Arabia'],
+                        'Stake': ['England', 'England2', 'England3', 'England4', 'Germany', 'Germany2', 'Spain', 'Spain2', 'Italy', 'Italy2', 'France', 'France2', 'Netherlands', 'Netherlands2', 'Portugal', 'Portugal2', 'Argentina', 'Austria', 'Australia', 'Brazil', 'Brazil2', 'Chile', 'China', 'Denmark', 'Japan', 'Japan2', 'Mexico', 'Norway', 'Saudi Arabia', 'Scotland', 'Sweden', 'Switzerland', 'Turkey', 'Usa', 'Croatia'],
+                        'Betclic': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Turkey', 'Switzerland'],
+                        'Winamax': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Switzerland', 'Turkey']
+                        }
+        
+        champ_list = []
+        for book in user_bookies:
+            for _ in champ_by_bookie[book]:
+                if _ not in champ_list:
+                    champ_list.append(_)
+        missed = df[~df['Pays'].isin(champ_list)]
         lexique = pd.DataFrame(
                     {'Alice Naming': ['Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 
                                     'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15'],
@@ -110,7 +138,7 @@ def main():
                                     'Over 2.5 goal', 'Under 2.5 goal', 'Both Team To Score', 'One Or Both Team To Not Score',
                                     'Home win & Over 1.5 goal', 'Away win & over 1.5 goal']}
                 )
-
+        
         trans = pd.DataFrame(
                     {'Naming': ['MP', 'W', 'D', 'L', 'GF', 
                                     'GA', 'GD', 'GFPG', 'GAPG', 'AS', 'DS'],
@@ -134,17 +162,6 @@ def main():
             else:
                 round_cols = ['Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']
                 df[round_cols] = df[round_cols].round(2)
-                champ_by_bookie = {'Unibet': ['Germany', 'Germany2', 'England', 'England2', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Scotland', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Portugal2', 'Sweden', 'Switzerland', 'Turkey', 'Saudi Arabia'],
-                        'Stake': ['England', 'England2', 'England3', 'England4', 'Germany', 'Germany2', 'Spain', 'Spain2', 'Italy', 'Italy2', 'France', 'France2', 'Netherlands', 'Netherlands2', 'Portugal', 'Portugal2', 'Argentina', 'Austria', 'Australia', 'Brazil', 'Brazil2', 'Chile', 'China', 'Denmark', 'Japan', 'Japan2', 'Mexico', 'Norway', 'Saudi Arabia', 'Scotland', 'Sweden', 'Switzerland', 'Turkey', 'Usa', 'Croatia'],
-                        'Betclic': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Turkey', 'Switzerland'],
-                        'Winamax': ['Germany', 'Germany2', 'England', 'England2', 'Saudi Arabia', 'Argentina', 'Australia', 'Austria', 'Belgium', 'Brazil', 'Chile', 'China', 'Croatia', 'Denmark', 'Spain', 'Spain2', 'Usa', 'France', 'France2', 'Italy', 'Italy2', 'Japan', 'Mexico', 'Norway', 'Netherlands', 'Portugal', 'Sweden', 'Switzerland', 'Turkey']
-                        }
-        
-                champ_list = []
-                for book in user_bookies:
-                    for _ in champ_by_bookie[book]:
-                        if _ not in champ_list:
-                            champ_list.append(_)
         
                 df = sp.filter_df(df, sp.create_widgets(df[['Pays', 'Proba_H', 'Proba_D', 'Proba_A', 'Proba_HD', 'Proba_DA', 'Proba_O', 'Proba_U', 'Proba_BTTS', 'Proba_NoBTTS', 'Proba_Ho15', 'Proba_Ao15']], {'Pays': 'multiselect'}))
                 
@@ -193,44 +210,25 @@ def main():
             if user_picks.empty or user_picks['Date'][0] != datetime.datetime.now().date().strftime('%d-%m-%y'):
                 st.write("There's no matchs for you today!")
             else:
-                bk = st.text_input('Enter your Bankroll:').replace(',', '.')
+                bk = st.text_input('Enter your Bankroll:')
                 try:
                     bk = float(bk)
                     for index, row in user_picks.iterrows():
                         odd = 0
                         for book in user_bookies:
-                            try:
-                                odds = float(row[f'Odds_{book}'])
-                            except:
-                                odds = 0
+                            odds = float(row[f'Odds_{book}'])
                             if odds > odd:
                                 user_picks.at[index, 'Odd'] = odds
                                 user_picks.at[index, 'Bookmaker'] = book
+
                     for index, row in user_picks.iterrows():
-                        if float(row['Odd']) >= 1.1:
-                            st.link_button(label= f"{row['Time']}: {row['Home']} vs {row['Away']}, bet {round(bk*(float(row['Coeff'])/100), 2)}€ on {row['Bets']} @ {row['Odd']}. Bookmaker is {row['Bookmaker']}", url= f"{row[f'{book}_Url']}")
+                        st.link_button(label= f"{row['Time']}: {row['Home']} vs {row['Away']}, bet {round(bk*(float(row['Coeff'])/100), 2)}€ on {row['Bets']} @ {row['Odd']}. Bookmaker is {row['Bookmaker']}", url= f"{row[f'{book}_Url']}")
                 except ValueError:
                     st.warning('Veuillez entrer une Bankroll valide')
-            col1, col2 = st.columns(2)
-            with col1:
+                    
                 if st.checkbox(label='Show Bets Signification'):
                     st.markdown(lexique.style.hide(axis="index").to_html(), unsafe_allow_html=True)
-            with col2:
-                url = "https://docs.google.com/spreadsheets/d/1k-khn63iYWNiDsC9iHVWlQXl1T_LZ1sE7doEptvJtrU/gviz/tq?tqx=out:csv&sheet=Tracking"        
-                historical = pd.read_csv(url, dtype=str)[['Date', 'Home', 'Away', 'Country', 'Bet', 'Odds', 'Stake', 'Result', 'Profit', 'BK']].dropna()
-                historical['Date'] = pd.to_datetime(historical['Date'], format='%d/%m/%Y')
-                user_join_date = pd.to_datetime(user_join, format='%d-%m-%y')
-                historical = historical[pd.to_datetime(historical['Date']) > pd.to_datetime(user_join_date)]
-                histo_bk = historical['BK'].to_list()
-                start = float(histo_bk[0].replace(',','.'))
-                last = float(histo_bk[-1].replace(',','.'))
-                histo_bk = [(float(value.replace(',', '.')) / start) * 100 for value in histo_bk]
-                plotly_fig = px.line(historical, y=histo_bk, title=f'{user_name} Portfolio performance since {user_join}')
-                plotly_fig.update_yaxes(title_text='Portfolio')
-                plotly_fig.update_xaxes(title_text='Number of Bets')
-                    
-                st.plotly_chart(plotly_fig)
-                st.write(f"Alice generated you a {round(last-start, 2)}% ROI in {len(historical)} bets, with an average win rate of {(round(float(len(historical[historical['Result'] == '1'])) / float(len(historical)), 2)) * 100}%!!")
+        
         elif choice == 'Advanced Statistics':
             datas = get_datas('alicedb', 'leaguesdb').drop(['_id'], axis=1).fillna(0)
             df['Match'] = '[' + df['Pays'] + '] / ' + df['Home'] + ' - ' + df['Away']
@@ -252,43 +250,59 @@ def main():
                 dff = pd.DataFrame(data)
                 for col in [shome, saway]:
                     dff[col] = dff[col].apply(lambda x: f"{x:.2f}")
+                st.subheader('Alice Predictions')
+                temp = df[df['Home'] == shome]
+                dic = pd.DataFrame({'Proba_Home': [temp['Proba_H'].values[0]], 'Proba_Draw': [temp['Proba_D'].values[0]], 'Proba_Away': [temp['Proba_A'].values[0]], 'Proba_HD': [temp['Proba_HD'].values[0]], 'Proba_DA': [temp['Proba_DA'].values[0]], 'Proba_Over': [temp['Proba_O'].values[0]], 'Proba_Under': [temp['Proba_U'].values[0]], 'Proba_BTTS': [temp['Proba_BTTS'].values[0]], 'Proba_NoBTTS': [temp['Proba_NoBTTS'].values[0]]})
+                for col in ['Proba_Home', 'Proba_Draw', 'Proba_Away', 'Proba_HD', 'Proba_DA', 'Proba_Over', 'Proba_Under', 'Proba_BTTS', 'Proba_NoBTTS']:
+                    dic[col] = dic[col].apply(lambda x: f"{x:.2f}")
                 
+                st.markdown(dic.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+                add_space(3)
                 col1, col2 = st.columns(2)
                 with col1:
+                    st.subheader('Statistics')
+                    add_space(3)
                     st.markdown(dff.style.hide(axis='index').to_html(), unsafe_allow_html=True)
                     if st.checkbox(label='Show Bets Signification'):
                         st.markdown(trans.style.hide(axis="index").to_html(), unsafe_allow_html=True)
                 with col2:
-                    choice = st.selectbox("Select a Bookmaker", ['Stake', 'Winamax'])
-
-                    if choice == 'Stake':
-                        st.link_button(label='Match Url', url= 'https://stake.bet/fr/sports/soccer/norway/eliteserien/44338223-hamarkameratene-bodoe-glimt')
-                        odsr = pd.DataFrame([{'Home': '6.20', 'Draw': '4.80', 'Away': '1.41'}])
-                        st.markdown(odsr.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
-                        odsd = pd.DataFrame([{'Home/Draw': '2.70', 'Draw/Away': '1.12'}])
-                        st.markdown(odsd.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
-                        odso = pd.DataFrame([{'Over 2.5': '2.43', 'Under 2.5': '1.54'}])
-                        st.markdown(odso.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
-                        odsb = pd.DataFrame([{'BTTS': '1.72', 'NoBTTS': '2.01'}])
-                        st.markdown(odsb.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
-                    elif choice == 'Winamax':
-                        st.link_button(label='Match Url', url= 'https://www.winamax.fr/paris-sportifs/match/46374315')
-                        odsr = pd.DataFrame([{'Home': '6.15', 'Draw': '4.67', 'Away': '1.38'}])
-                        st.markdown(odsr.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
-                        odsd = pd.DataFrame([{'Home/Draw': '2.68', 'Draw/Away': '1.1'}])
-                        st.markdown(odsd.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
-                        odso = pd.DataFrame([{'Over 2.5': '2.37', 'Under 2.5': '1.5'}])
-                        st.markdown(odso.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
-                        odsb = pd.DataFrame([{'BTTS': '1.68', 'NoBTTS': '1.98'}])
-                        st.markdown(odsb.style.hide(axis='index').to_html(), unsafe_allow_html=True)
-                        st.write('')
+                    odds_df = get_datas('alicedb', 'odds_df').drop('_id', axis=1)
+                    affi = {'Stake': 'https://stake.bet/?c=7AcXNyMl', 'Unibet': 'https://unibet.fr/myaccount/register.html?promo=LFQRK'}
+                    odds_df = odds_df[odds_df['Home'] == shome]
+                    st.subheader('Odds')
+                    if not st.toggle('Switch to All Odds'):
+                        for bet in ['Home', 'Draw', 'Away', 'HD', 'DA', 'Over', 'Under', 'BTTS', 'NoBTTS']:
+                            od, od_url, bookie = get_max(odds_df, bet)
+                            if bookie not in user_bookies and bookie in affi.keys():
+                                st.link_button(label=f"{bet} @ {od} on {bookie}", url=affi[bookie])
+                            else:
+                                st.link_button(label=f"{bet} @ {od} on {bookie}", url=odds_df[od_url].values[0])
+                    else:
+                        for bet in ['Home', 'Draw', 'Away', 'HD', 'DA', 'Over', 'Under', 'BTTS', 'NoBTTS']:
+                            st.subheader(bet)
+                            co1, co2, co3, co4 = st.columns(4)
+                            with co1:
+                                val = odds_df[f'Stake_{bet}'].values[0]
+                                st.write('Stake')
+                                if 'Stake' not in user_bookies:
+                                    st.link_button(label=f'{val}', url=affi['Stake'])
+                                else:
+                                    st.link_button(label=f'{val}', url= odds_df['Stake_Url'].values[0])
+                            with co2:
+                                val = odds_df[f'Unibet_{bet}'].values[0]
+                                st.write('Unibet')
+                                if 'Unibet' not in user_bookies:
+                                    st.link_button(label=f'{val}', url=affi['Unibet'])
+                                else:
+                                    st.link_button(label=f'{val}', url= odds_df['Unibet_Url'].values[0])
+                            with co3:
+                                val = odds_df[f'Winamax_{bet}'].values[0]
+                                st.write('Winamax')
+                                st.link_button(label=f'{val}', url= odds_df['Winamax_Url'].values[0])
+                            with co4:
+                                val = odds_df[f'Betclic_{bet}'].values[0]
+                                st.write('Betclic')
+                                st.link_button(label=f'{val}', url= odds_df['Betclic_Url'].values[0])
 
                 gr1, gr2 = st.columns(2)
                 with gr1:
@@ -341,6 +355,8 @@ def main():
                         width=400,
                     )
                     st.plotly_chart(fig)
+
+
+
 if __name__ == '__main__':
     main()
-
